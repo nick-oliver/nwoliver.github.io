c669e60588b4b7ca80d8ea379a3f810ed0a1a6bc
Nick Oliver <nick.oliver@rubydev.safehold.local>
2019-10-05 09:01:08 -0500

    final gallery commit

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,30 @@
+// Activates the Image gallery
+// The main task is to attach an event listener to each image in the Gallery
+// and respond appropriately on click
+
 function activateGallery() {
-  alert("Hello from the Gallery file!!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+
+      // Set clicked image as the main image
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // change which image is current
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image description column
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
